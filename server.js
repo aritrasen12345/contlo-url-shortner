a983aa9b0d44279d40f99dd1fb3343239ee309dd
Aritra Sen <aritrasen12345@gmail.com>
2023-04-28 23:39:42 +0530

    Final Commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const DB = config.DB_URL.replace("<password>", config.DB_PASSWORD);
 
 // DB CONNECTION
 const options = {
-  // autoIndex: false, // Don't build indexes
+  // autoIndex: false, // Don't build indexe
   maxPoolSize: 10, // Maintain up to 10 socket connections
   serverSelectionTimeoutMS: 10000, // Keep trying to send operations for 5 seconds
   // socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
